c31c73bee4568346cc1e30f75c326b32d56711de
kaksha96 <kdp55@njit.edu>
2018-10-23 17:42:17 -0400

    Task : added all files and directory in views

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+
+@include('includes.head')
+
+
+<body>
+
+@include('includes.menu')
+
+@yield('content')
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+@include('includes.jsfooter')
+
+
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+
+
+@extends('layouts.default')
+
+@section('content')
+
+
+@endsection
+
+
+<h1>Hello, world!</h1>
+
+
+
