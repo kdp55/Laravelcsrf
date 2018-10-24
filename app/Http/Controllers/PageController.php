83a220e59e0bdee50885ca7953dfdf78ed96abae
kaksha96 <kdp55@njit.edu>
2018-10-24 01:00:50 -0400

    Fix : mirror bugs solved

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,7 +12,7 @@ class PageController extends Controller{
         return view('pages.contact');
     }
     function store(Request $request){
-        $name = $request->name;
+        $name = $request-> $name;
         return redirect()->route('thanks',['name' => $name]);
     }
     function thanks($name, Request $request){
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.default')
 
 @section('content')
-
+<div clas="container">
     <h1>This is KDP page. Welcome Here!</h1>
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-
+</div>
 
 @endsection
